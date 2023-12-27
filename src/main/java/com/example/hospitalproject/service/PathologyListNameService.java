92abab7532dba500f8327e7678c016c4b96fc926
Mahadi-spring <mahadi.spring.developer@gmail.com>
2023-12-28 00:05:41 +0600

    updated

---
+++ src/main/java/com/example/hospitalproject/service/PathologyListNameService.java
@@ -0,0 +1,22 @@
+package com.example.hospitalproject.service;
+
+import com.example.hospitalproject.entity.PathologyList;
+import com.example.hospitalproject.repository.PathologyListNameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PathologyListNameService {
+    @Autowired
+    PathologyListNameRepository listNameRepository;
+    public List<PathologyList>pathologyLists(){
+        return listNameRepository.findAll();
+    }
+
+
+    public Integer listofTestAmount(String labTestName){
+        return listNameRepository.totalTestAmount(labTestName);
+    }
+}
